0439a42fe9963b03927fb9676184b3f344e6b6a0
Verwoerd <thomas@keystone-strategic.com>
2023-11-30 17:34:26 +0100

    Fix gradle 8.5 issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,14 @@
+
+buildscript {
+  repositories {
+    maven {
+      url = uri("https://plugins.gradle.org/m2/")
+    }
+  }
+  dependencies {
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+  }
+}
 rootProject.name = "aoc-template"
 include(
   "shared"
